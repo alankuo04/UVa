8192066f944eaa1cd6243009e3ee51cc0806ca11
alankuo04 <68884383+alankuo04@users.noreply.github.com>
2021-04-23 15:10:40 +0800

    update README.md

--- update.cpp
+++ update.cpp
@@ -14,13 +14,13 @@ int main(){
         star_list[s.substr(0, s.find(","))] = s.substr(s.find(",")+1, s.size());
     file.close();
     file.open("./README.md",ios::out);
-    file<<"# UVa題目練習\n##### 高等程式設計課程練習紀錄，以上題目皆為OnlineJudge上AC通過\n|難度|題號|完成日期|\n|---|----|--------|\n";
+    file<<"# UVa題目練習\n##### 高等程式設計課程練習紀錄，以上題目皆為OnlineJudge上AC通過\n|難度|題號|題目連結|\n|---|----|--------|\n";
     while(cin>>s){
         if(star_list.find(s)==star_list.end())
             cout<<"Unknown problem number."<<endl;
         else{
             string stars[6] = {"☆☆☆☆☆","★☆☆☆☆","★★☆☆☆","★★★☆☆","★★★★☆","★★★★"};
-            file<<"|"<<stars[stoi(star_list[s])]<<"|["<<s<<"](https://github.com/alankuo04/UVa/tree/main/"<<s<<")|"<<endl;
+            file<<"|"<<stars[stoi(star_list[s])]<<"|["<<s<<"](https://github.com/alankuo04/UVa/tree/main/"<<s<<")|["<<s<<"題目](https://onlinejudge.org/external/"<<s.substr(0,3)<<"/"<<s<<".pdf)"<<endl;
         }
     }
     file.close();
